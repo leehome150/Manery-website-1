c6b5ee5b4012e68955981fdd22e921f35148dfcd
leehome150 <750979206@qq.com>
2020-05-24 13:57:54 +0800

    Money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>Money.vue
+    <div class="nav-wrapper">
+        <div class="content">
+            Money.vue
+        </div>
     <Nav/>
     </div>
 </template>
@@ -10,4 +13,14 @@
 </script>
 
 <style lang="scss" scoped>
+    .nav-wrapper{
+        border:1px solid green;
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+    }
+    .content{
+        flex-grow: 1;
+        overflow: auto;
+    }
 </style>
