9762f2467ecde40ec9e98ce46cdcc9bfa8f660aa
leehome150 <750979206@qq.com>
2020-05-27 10:29:33 +0800

    把recode的复制保存到reLcodeist

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
     <Layout class-prefix="layout">
         {{recode}}
-        <NumberPad @update:value="onUpdateAmount"/>
+        <NumberPad @update:value="onUpdateAmount" @submit="saveRecode"/>
         <Types :value.sync="recode.type"/>
         <Notes @update:value="onUpdateNotes"/>
         <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
@@ -14,9 +14,9 @@
   import Types from '@/components/Money/Types.vue';
   import Notes from '@/components/Money/Notes.vue';
   import Tags from '@/components/Money/Tags.vue';
-  import {Component} from 'vue-property-decorator';
+  import {Component, Watch} from 'vue-property-decorator';
 
-  type Record ={
+  type Record = {
     tags: string[];
     notes: string;
     type: string;
@@ -27,18 +27,31 @@
   export default class Money extends Vue {
 
     tags = ['衣', '食', '住', '行', '娱乐'];
-    recode: Record= {
-      tags: [],notes: '',type: '-',amount:0
-    }
+    recodeList: Record[] = [];
+    recode: Record = {
+      tags: [], notes: '', type: '-', amount: 0
+    };
 
     onUpdateTags(value: string[]) {
-      this.recode.tags =value;
+      this.recode.tags = value;
     }
-    onUpdateNotes(value: string){
-      this.recode.notes=value;
+
+    onUpdateNotes(value: string) {
+      this.recode.notes = value;
     }
-    onUpdateAmount(value: string){
-      this.recode.amount=parseFloat(value);
+
+    onUpdateAmount(value: string) {
+      this.recode.amount = parseFloat(value);
+    }
+
+    saveRecode() {
+      const recode2=JSON.parse(JSON.stringify(this.recode))
+      this.recodeList.push(recode2)
+      console.log(this.recodeList);
+    }
+    @Watch('recodeList')
+    onRecodeListChanged(){
+      localStorage.setItem('recodeList',JSON.stringify(this.recodeList))
     }
   }
 </script>
