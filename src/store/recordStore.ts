44ec075f8390c517b7a2e802972f15cc58e61b15
leehome150 <750979206@qq.com>
2020-05-30 15:48:06 +0800

    模块化store,删除了model

---
+++ src/store/recordStore.ts
@@ -0,0 +1,26 @@
+import clone from '@/lib/clone';
+
+const localStorageKeyName = 'recordList';
+
+const recordStore = {
+  recordList: [] as RecordItem[],
+  fetchRecords() {
+    this.recordList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    return this.recordList;
+  },
+
+  saveRecords() {
+    window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+  },
+
+
+  createRecord(record: RecordItem) {
+    const recode2: RecordItem = clone(record);
+    recode2.createAt = new Date();
+    this.recordList?.push(recode2);
+    recordStore.saveRecords();
+  },
+};
+
+recordStore.fetchRecords();
+export default recordStore;
