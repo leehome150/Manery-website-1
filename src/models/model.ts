c4f483ab7dc9163931232044a544233ab9c169e8
leehome150 <750979206@qq.com>
2020-05-30 09:32:13 +0800

    重新封装Model

--- src/models/model.ts
+++ src/models/model.ts
@@ -1,9 +1,11 @@
 const model = {
+  data:[] as RecordItem[],
   fetch() {
-    return JSON.parse(window.localStorage.getItem('recodeList') || '[]') as RecordItem[];
+    this.data=JSON.parse(window.localStorage.getItem('recodeList') || '[]') as RecordItem[];
+    return this.data;
   },
-  save(data: RecordItem[]) {
-    window.localStorage.setItem('recodeList', JSON.stringify(data));
+  save() {
+    window.localStorage.setItem('recodeList', JSON.stringify(this.data));
   },
   clone(data: RecordItem[]|RecordItem){
     return JSON.parse(JSON.stringify(data))
