015f4777baf31d9834272535bc213a1f2d6bf460
leehome150 <750979206@qq.com>
2020-05-30 13:57:35 +0800

    将所有操作封装在store里

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -18,12 +18,16 @@ type TagListModel = {
   remove: (id: string) => boolean;
   save: () => void;
 }
+
 interface Window {
-  tagList: Tag[];
-  createTag: (name: string) => void;
-  findTag: (id: string) => Tag|undefined;
-  removeTag: (id: string) => boolean;
-  updateTag: (id: string,name: string) => 'success' | 'not_found' | 'duplicated';
-  recordList: RecordItem[];
-  createRecord: (record: RecordItem) => void;
+  store: {
+    tagList: Tag[];
+    createTag: (name: string) => void;
+    findTag: (id: string) => Tag | undefined;
+    removeTag: (id: string) => boolean;
+    updateTag: (id: string, name: string) => 'success' | 'not_found' | 'duplicated';
+    recordList: RecordItem[];
+    createRecord: (record: RecordItem) => void;
+  };
+
 }
