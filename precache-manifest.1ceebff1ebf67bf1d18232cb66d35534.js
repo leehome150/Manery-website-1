c6a4cdb1337083ff1ebe41e9b203100ae314d423
leehome150 <750979206@qq.com>
2020-07-02 13:05:49 +0800

    deploy

---
+++ precache-manifest.1ceebff1ebf67bf1d18232cb66d35534.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "accaa552397b58823379",
+    "url": "/Manery-website-1/css/app.53274ea3.css"
+  },
+  {
+    "revision": "ad14b768cdf6f1470a3ddf8b0ef77f55",
+    "url": "/Manery-website-1/index.html"
+  },
+  {
+    "revision": "accaa552397b58823379",
+    "url": "/Manery-website-1/js/app.0c21ae97.js"
+  },
+  {
+    "revision": "63796ac6813cc50fc636",
+    "url": "/Manery-website-1/js/chunk-vendors.c8145f41.js"
+  },
+  {
+    "revision": "ac8232fc584186e9c361c815d3f09418",
+    "url": "/Manery-website-1/manifest.json"
+  }
+]);
