77a46b07d9152a292a27106187444297d360d8f6
leehome <750979206@qq.com>
2021-11-23 11:36:54 +0800

    feat: 引入element ui

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,31 +1,30 @@
 <template>
-    <div class="button-wrapper">
-        <button class="button"
-                @click="$emit('click',$event)">
-            <slot/>
-        </button>
-    </div>
+  <div class="button-wrapper">
+    <el-button class="button" @click="$emit('click', $event)">
+      <slot />
+    </el-button>
+  </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Button'
-  };
+export default {
+  name: "Button",
+};
 </script>
 
 <style lang="scss" scoped>
-    .button {
-        background: #767676;
-        color: white;
-        border-radius: 4px;
-        border: none;
-        height: 40px;
-        padding: 0 16px;
+.button {
+  background: #767676;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding: 0 16px;
 
-        &-wrapper {
-            text-align: center;
-            padding: 16px;
-            margin: 28px;
-        }
-    }
+  &-wrapper {
+    text-align: center;
+    padding: 16px;
+    margin: 28px;
+  }
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,24 @@
-import Vue from 'vue';
-import App from './App.vue';
-import './registerServiceWorker';
-import router from './router';
-import store from './store';
-import Nav from '@/components/Nav.vue';
-import Layout from '@/components/Layout.vue';
-import Icon from '@/components/Icon.vue';
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import ElementUI from "element-ui";
+import router from "./router";
+import store from "./store";
+import Nav from "@/components/Nav.vue";
+import Layout from "@/components/Layout.vue";
+import Icon from "@/components/Icon.vue";
 
 Vue.config.productionTip = false;
-
-Vue.component('Nav', Nav);
-Vue.component('Layout', Layout);
-Vue.component('Icon', Icon);
+Vue.use(ElementUI);
+Vue.component("Nav", Nav);
+Vue.component("Layout", Layout);
+Vue.component("Icon", Icon);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app');
-
+  render: (h) => h(App),
+}).$mount("#app");
 
 // if (document.documentElement.clientWidth > 500) {
 //   window.alert('请使用手机手机打开本页面，以保证浏览效果');
@@ -32,4 +32,3 @@ new Vue({
 //   document.body.appendChild(img);
 //
 // }
-
