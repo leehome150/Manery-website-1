2b46bd1e276f01275929efdf5c3dcbf738be6c56
leehome <750979206@qq.com>
2021-11-24 10:59:35 +0800

    deploy

---
+++ precache-manifest.5bc778befa47bd62311b8c8edb2312ca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d9d481198a2d0cfdfec",
+    "url": "/Manery-website-1/css/app.31fda0f2.css"
+  },
+  {
+    "revision": "ab90b57768143f7b76f9",
+    "url": "/Manery-website-1/css/chunk-vendors.4a2faee0.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/Manery-website-1/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/Manery-website-1/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "7b622fe7f5d7a02114d3facef0f9b436",
+    "url": "/Manery-website-1/index.html"
+  },
+  {
+    "revision": "5d9d481198a2d0cfdfec",
+    "url": "/Manery-website-1/js/app.608e6ad8.js"
+  },
+  {
+    "revision": "ab90b57768143f7b76f9",
+    "url": "/Manery-website-1/js/chunk-vendors.0848764d.js"
+  },
+  {
+    "revision": "ac8232fc584186e9c361c815d3f09418",
+    "url": "/Manery-website-1/manifest.json"
+  }
+]);
